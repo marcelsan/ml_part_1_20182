d043012f7e33b000071e20394c796c1b3565f413
Marcel Santana <mss8@cin.ufpe.br>
2018-10-24 10:07:58 -0300

    Add the division by determinant on the likelihood calculus

--- models/BayesianClassifier.py
+++ models/BayesianClassifier.py
@@ -2,12 +2,14 @@ import sys
 
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.naive_bayes import GaussianNB
 
 class BayesianClassifier(BaseEstimator, ClassifierMixin):
 	""" Gaussian Bayesian classifier """
 	
 	def __init__(self, arg=None):
 		self.arg = arg
+		self.fitted = False
 
 	def fit(self, X, y):
 		"""
@@ -30,10 +32,10 @@ class BayesianClassifier(BaseEstimator, ClassifierMixin):
 
 		classes = np.unique(y)
 		n_classes = len(classes)
-		n_features = X.shape[1]
+		self.n_features_ = X.shape[1]
 
-		self.theta_ = np.zeros((n_classes, n_features))
-		self.sigma_ = np.zeros((n_classes, n_features))
+		self.theta_ = np.zeros((n_classes, self.n_features_))
+		self.sigma_ = np.zeros((n_classes, self.n_features_))
 		self.class_count_ = np.zeros_like(classes)
 
 		for c in classes:
@@ -46,6 +48,9 @@ class BayesianClassifier(BaseEstimator, ClassifierMixin):
 			self.sigma_[c, : ] = np.mean(np.square(X_c - mu_c), axis=0)
 			self.class_count_[c] = N_c
 		
+		self.pw_ = self.class_count_ / np.sum(self.class_count_)
+		self.fitted = True
+
 		return self
 
 	def predict(self, X):
@@ -67,15 +72,38 @@ class BayesianClassifier(BaseEstimator, ClassifierMixin):
 
 		assert(X.shape[1] == self.theta_.shape[1])
 
-		C = np.zeros((X.shape[0], ), dtype=np.uint)
-		P_w = self.class_count_ / np.sum(self.class_count_)
+		C = np.zeros((X.shape[0], ), dtype=np.uint)	
 
 		sigma_inv = 1/(self.sigma_ + 0.00001)
+		det_sigma_inv_abs = np.abs(np.prod(sigma_inv, axis=1))
+		A = 1/np.sqrt(np.power(2 * np.pi, self.n_features_) * (det_sigma_inv_abs))
 
 		for i, x_k in enumerate(X):
-			C[i] = np.argmax(np.exp((-1/2) * np.sum((x_k - self.theta_) * sigma_inv * (x_k - self.theta_), axis=1)) * P_w)
+			C[i] = np.argmax(A * np.exp((-1/2) * np.sum((x_k - self.theta_) * sigma_inv * (x_k - self.theta_), axis=1)) * self.pw_)
 
 		return C
+
+	def predict_proba(self, X):
+		"""
+		Return probability estimates for the test data X.
+
+		Parameters
+		---------
+		X : array of shape (n_samples, n_features)
+			Array of test vectors.
+
+
+		Returns
+		---------
+		p : array of shape (n_samples, n_classes)
+			The class probabilities of the input sample.
+			
+		"""
+
+		if self.fitted is False:
+			raise RuntimeError('The estimator was not fittet. Consider call .fit() first.')
+
+
 
 
 def main(argv):
@@ -85,7 +113,11 @@ def main(argv):
 	bc = BayesianClassifier()
 	bc.fit(X, Y)
 
+	clf = GaussianNB()
+	clf.fit(X, Y)
+
 	print(bc.predict(np.array([[-0.8, -1]])))
+	print(clf.predict(np.array([[-0.8, -1]])))
 
 if __name__ == "__main__":
     main(sys.argv)
